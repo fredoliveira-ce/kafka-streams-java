5e66bfdc5b2d38a6785dbaf3486467dfe17f5717
Fred Junior <fredoliveira.ce@gmail.com>
2022-12-10 12:28:03 +0100

    Implement word count.

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # kafka-streams-java
+```kafka-topics --bootstrap-server localhost:9092 --create --topic word-count-input --partitions 2 --replication-factor 1```
+```kafka-topics --bootstrap-server localhost:9092 --create --topic word-count-output --partitions 2 --replication-factor 1```
+
+Check it out
+```kafka-topics --bootstrap-server localhost:9092 --list```
+
+Kafka consumer
+```
+kafka-console-consumer --bootstrap-server localhost:9092 \
+        --topic word-count-output \
+        --formatter kafka.tools.DefaultMessageFormatter \
+        --property print.timestamp=true \
+        --property print.key=true \
+        --property print.value=true \
+        --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
+        --property value.deserializer=org.apache.kafka.common.serialization.LongDeserializer \
+        --from-beginning
+```
+
+Kafka producer
+```
+kafka-console-producer --bootstrap-server localhost:9092 --topic word-count-input
+```
---
+++ src/main/java/com/github/fredoliveira/wordcount/WordCount.java
@@ -0,0 +1,43 @@
+package com.github.fredoliveira.wordcount;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Named;
+import org.apache.kafka.streams.kstream.Produced;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+public class WordCount {
+
+    public static void main(String[] args) {
+        Properties config = new Properties();
+        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
+        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder();
+        KStream<String, String> stream = builder.stream("word-count-input");
+        KTable<String, Long> wordCount = stream
+                .mapValues(text -> text.toLowerCase())
+                .flatMapValues(value -> Arrays.asList(value.split(" ")))
+                .selectKey((key, word) -> word)
+                .groupByKey()
+                .count(Named.as("counts"));
+
+        wordCount.toStream().to("word-count-output", Produced.with(Serdes.String(), Serdes.Long()));
+
+        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+        streams.start();
+        System.out.println(streams);
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+
+}
